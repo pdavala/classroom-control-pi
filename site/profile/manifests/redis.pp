49c190843177b098a871aef403f431bfb013a24b
Shobhna Shastri <shobhna@puppet.com>
2016-10-26 09:07:37 +1100

    Added containment statement

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,7 +1,7 @@
 class profile::redis {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
-  require profile::epel
+  contain profile::epel
   
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
@@ -9,4 +9,5 @@ class profile::redis {
     
   }
   
+  contain redis
 }
