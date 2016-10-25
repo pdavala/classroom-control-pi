dbb2cc68d3af04aa11f84084237384a171eb6093
pdavala <poornima.davala@jpmchase.com>
2016-10-26 06:35:59 +0800

    memory statementadded

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -2,6 +2,7 @@ class profile::redis {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
   require profile::epel
+  maxmemory => '10mb'
   
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
