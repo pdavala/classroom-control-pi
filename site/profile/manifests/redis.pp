f7d42c0b27ece8a91adc09575a9c40e7d4091145
jsmall3 <jsmall3@puppetlabs.vm>
2016-10-26 10:00:18 +0000

    LAB-11.1 redis master param

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,4 +1,6 @@
-class profile::redis {
+class profile::redis (
+  Optional[Boolean] $master = false
+  ) {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
   require profile::epel
@@ -6,10 +8,18 @@ class profile::redis {
   # process all yum repos prior to any pkgs in this class
   Yumrepo <||> -> Package <| tag == 'profile::redis' |>
 
+  if $master {
+    $slaveof = undef;
+  }
+  else {
+    $slaveof = 'master.puppetlabs.vm 6479'
+  }
+
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
     maxmemory => '10mb',
-
+    bind => $::ipaddress,
+    slaveof => $slaveof,
   }
 
 }
