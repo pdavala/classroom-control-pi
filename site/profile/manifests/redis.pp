7ce3fe5e34d43e6ec17d8752b96005669221dc43
jsmall3 <jsmall3@puppetlabs.vm>
2016-10-26 03:50:07 +0000

    LAB-6.2 resource ordering

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -2,6 +2,9 @@ class profile::redis {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
   require profile::epel
+
+  # process all yum repos prior to any pkgs in this class
+  Yumrepo <||> -> Package <| tag == 'profile::redis' |>
   
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
