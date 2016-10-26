469eeae57e124a0f8820c3fd55b20b37b709a7cd
irplagura <irplagura@puppetlabs.vm>
2016-10-26 10:39:04 +0000

    add windows reg

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -7,4 +7,15 @@ class profile::tuning::windows {
 
   # Add more registry resources as directed
   
+    registry::value { 'KeepAliveInterval':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+      value  => 'TcpMaxDataRetransmissions',
+      data   => '5',
+    }
+
+    registry::value { 'KeepAliveInterval':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+      value  => 'TcpTimedWaitDelay',
+      data   => '0x0000001e',
+    }
 }
