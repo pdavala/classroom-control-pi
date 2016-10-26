1e643eeca7359eec59c213ed718fe6ebb4995499
pdavala <poornima.davala@jpmchase.com>
2016-10-26 13:08:42 +0800

    Update managed_user.pp

--- site/skeleton/manifests/managed_user.pp
+++ site/skeleton/manifests/managed_user.pp
@@ -43,8 +43,11 @@ define skeleton::managed_user (
     }
 
     # TODO: Add a file resource to manage "${homedir}/.bashrc"
-
-  }
+  file{"${homedir}/.bashrc":
+  ensure => file,
+  source => 'puppet:///modules/skeleton/bashrc',
+  
+    }
 
   # Puppet will evaluate these resources in the proper order because it's smart
   # and knows about dependencies between files and their owners
