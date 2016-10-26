6ff0e7a7c0ea76bc1afe101d297dbc1ff46d955b
pdavala <poornima.davala@jpmchase.com>
2016-10-26 11:41:09 +0800

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,16 +1,23 @@
- class profile::redis ( $master = false,) {
+class profile::redis ($master = false) {
+
+class profile::redis {
+  # We'll require our epel class so that it is enforced first.
+  # Don't forget to update that class before enforcing your code
   require profile::epel
+  
   if $master {
-$slaveof = undef
+      $slaveof = undef
   }
-else
- {
-$slaveof = 'master.puppetlabs.vm 6479'
+  else {
+    $slaveof = 'master.puppetlabs.vm 6479'
   }
-class {'redis':
-maxmemory => '10mb',
-bind  => $ipaddress,
-slaveof = $slaveof,
+  
+  class { 'redis':
+    # what parameter should we pass to set maxmemory to 10mb?
+    maxmemory => '10mb',
+    bind      => $ipaddress,
+    slaveof => $slaveof,
+  }
+  contain redis
 }
 }
-   
