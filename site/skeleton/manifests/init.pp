45213c4f4b4631ed1c0e8dea2bc6c588fec0a007
pdavala <poornima.davala@jpmchase.com>
2016-10-26 10:36:15 +0800

    updated the skel dir

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,12 +1,19 @@
 class skeleton {
   file { '/etc/skel':
 #    ensure => ???,   # what value should go here?
+    ensure => 'dir',
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
   
   # add a resource to manage /etc/skel/.bashrc
-
-
+   file { '/etc/skel/.bashrc':
+    ensure => 'file',
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    content => "This is poornimas bashrc file",
+    
+    }
 }
