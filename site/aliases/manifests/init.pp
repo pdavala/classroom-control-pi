2aa71c3fdb805f6d4b331a428a39336ae74d2988
pdavala <poornima.davala@jpmchase.com>
2016-10-27 07:22:44 +0800

    Update init.pp

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -2,13 +2,13 @@ class aliases (
     $admin = 'root',
 ) {
     # uses $admin to build the aliases file
-    # TODO: Add the function call to generate text from a template
+   
     file { '/etc/aliases':
         ensure  => file,
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => '???', # What should go here?
+        content => epp('aliases/aliases.epp',{admin => $admin}),
     }
     exec { '/usr/bin/newaliases':
         refreshonly => true,
