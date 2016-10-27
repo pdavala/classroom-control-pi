bda07afef8a06f141f0874026c1327a01393ba8b
jsmall3 <jsmall3@puppetlabs.vm>
2016-10-27 05:23:47 +0000

    LAB-12.4 defined types

--- site/skeleton/manifests/managed_user.pp
+++ site/skeleton/manifests/managed_user.pp
@@ -1,7 +1,6 @@
 define skeleton::managed_user (
+  $password,
   $home = undef,
-  # TODO: Add a password parameter
-  
 ) {
   if $home {
     $homedir = $home
@@ -42,7 +41,10 @@ define skeleton::managed_user (
       mode  => '0644',
     }
 
-    # TODO: Add a file resource to manage "${homedir}/.bashrc"
+    file { "${homedir}/.bashrc":
+      ensure => file,
+      source => 'puppet:///modules/skeleton/dotbshrc',
+    }
 
   }
 
@@ -52,8 +54,8 @@ define skeleton::managed_user (
   user { $name:
     ensure     => present,
     managehome => true,
-    # TODO: Pass the password parameter to this resource
-    
+    password   => $password,
+
   }
 
   file { $homedir:
