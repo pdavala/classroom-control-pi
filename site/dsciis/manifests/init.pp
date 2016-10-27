f9de0e4186212dd0bcc25fee9ba25a217b30de99
jsmall3 <jsmall3@puppetlabs.vm>
2016-10-27 07:44:04 +0000

    meh...

--- site/dsciis/manifests/init.pp
+++ site/dsciis/manifests/init.pp
@@ -6,28 +6,20 @@ class dsciis {
     timeout => 15,
   }
 
-  # Translate these DSC Powershell resources into Puppet code below
-#  WindowsFeature iis {
   dsc_windowsfeature {'iis':
-#    Ensure    = 'Present'
     dsc_ensure => 'Present',
-#    Name      = 'Web-Server'
     dsc_name   => 'Web-Server',
     }
 
-#  WindowsFeature iisscriptingtools {
   dsc_windowsfeature {'iisscriptingtools': 
-#    Ensure    = 'Present',
     dsc_ensure => 'Present',
-#    Name      = 'Web-Scripting-Tools',
     dsc_name => 'Web-Scripting-Tools',
   }
 
-  # The index file is managed as a native Puppet file resource.
   file { 'C:/inetpub/wwwroot/index.html':
     ensure => 'file',
     source => 'puppet:///modules/dsciis/index.html',
-#    require => Dsc_windowsfeature['iis'],
+    require => Dsc_windowsfeature['iis'],
   }
 
 }
